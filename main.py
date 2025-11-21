ef62cd6312c4e570e6a7339a9c9e5e2ce13128f4
Waleed Amgad <waleedbookey@gmail.com>
2025-11-21 12:54:42 +0200

    testing

--- main.py
+++ main.py
@@ -45,20 +45,38 @@ def main():
     imgui.create_context()
     renderer = PygameRenderer()
 
-    # OpenGL setup
+    # OpenGL Lighting 
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
     glEnable(GL_COLOR_MATERIAL)
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
     glShadeModel(GL_SMOOTH)
 
+    #init Camera
     CameraPos = [0, -3, -20]
-
+    
     gluPerspective(45, display[0] / display[1], 0.1, 50.0)
     glTranslatef(*CameraPos)
     glRotatef(45, 1, 0, 0)
 
+    #Bool vars
     Rotated = True
+
+    #Element 
+
+    #Positons 
+
+    LightSourcePos=[0,20,0] 
+    PlayerPos=[0,0,0] 
+
+    #Bool for checks 
+    Rotated=True 
+
+    #Timers 
+
+    #Debug vars 
+    ShowDevWindow=False
+
 
     while True:
 
@@ -66,10 +84,15 @@ def main():
         for event in pygame.event.get():
             renderer.process_event(event)
             if event.type == pygame.QUIT:
-                ExitFunc
+                ExitFunc()
                 break
 
             if event.type == pygame.KEYDOWN:
+                if event.key ==pygame.K_F12:
+                    if not ShowDevWindow:
+                        ShowDevWindow=True
+                    else:
+                        ShowDevWindow=False
                 if event.key == pygame.K_ESCAPE:
                     ExitFunc()
                     break
@@ -87,22 +110,23 @@ def main():
                         glRotatef(-45, 1, 0, 0)
                         Rotated = False
 
-        # -----------------------
-        # Update ImGui screen size
+            # -----------------------
+            # Update ImGui screen size
         w, h = pygame.display.get_surface().get_size()
         imgui.get_io().display_size = (w, h)
 
         imgui.new_frame()
 
-        # UI (must have begin/end)
-        imgui.begin("Debug Window")
-        imgui.text("This is a test window")
-        if imgui.button("Click me"):
-            print("hello world")
-        imgui.end()
-        # -----------------------
+        if ShowDevWindow==True:
+            # UI (must have begin/end)
+            imgui.begin("Debug Window")
+            imgui.text("This is a test window")
+            if imgui.button("Click me"):
+             print("hello world")
+            imgui.end()
+            # -----------------------
 
-        # Clear screen *before* ImGui
+            # Clear screen *before* ImGui
         glClearColor(135/255, 206/255, 235/255, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
@@ -115,7 +139,6 @@ def main():
         pygame.display.flip()
         pygame.time.wait(10)
 
-    # Shutdown outside loop
 
 
 main()
